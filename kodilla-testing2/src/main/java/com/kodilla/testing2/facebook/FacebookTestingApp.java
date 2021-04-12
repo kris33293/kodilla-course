ff46728afb18cf6366251b2791a56a72c17e67a6
Rafal Krismann <rkrismann33@gmail.com>
2021-04-12 21:51:51 +0100

    32.2

--- kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/facebook/FacebookTestingApp.java
@@ -11,8 +11,14 @@ public class FacebookTestingApp {
 
     public static final String XPATH_COOKIES = "//button[2]";
     public static final String XPATH_CREATE_ACCOUNT = "//div[contains(@class, \"_6ltg\")]/a";
-    public static final String XPATH_FIRST_NAME = "class=inputtext _58mg _5dba _2ph-";
-    public static void main(String[] args) {
+    public static final String XPATH_FIRST_NAME = "/html/body/div[3]/div[2]/div/div/div[2]/div/div/div[1]/form/div[1]/div[1]/div[1]/div[1]/div/input";
+    public static final String XPATH_LAST_NAME = "/html/body/div[3]/div[2]/div/div/div[2]/div/div/div[1]/form/div[1]/div[1]/div[1]/div[2]/div/div[1]/input";
+    public static final String XPATH_EMAIL = "/html/body/div[3]/div[2]/div/div/div[2]/div/div/div[1]/form/div[1]/div[2]/div/div[1]/input";
+    public static final String XPATH_DAY_OF_BIRTH = "/html/body/div[3]/div[2]/div/div/div[2]/div/div/div[1]/form/div[1]/div[5]/div[2]/span/span/select[2]";
+    public static final String XPATH_MONTH_OF_BIRTH = "/html/body/div[3]/div[2]/div/div/div[2]/div/div/div[1]/form/div[1]/div[5]/div[2]/span/span/select[1]";
+    public static final String XPATH_YEAR_OF_BIRTH = "/html/body/div[3]/div[2]/div/div/div[2]/div/div/div[1]/form/div[1]/div[5]/div[2]/span/span/select[3]";
+
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
         driver.get("https://facebook.com");
 
@@ -23,9 +29,29 @@ public class FacebookTestingApp {
         while (!driver.findElement(By.xpath(XPATH_CREATE_ACCOUNT)).isDisplayed());
         driver.findElement(By.xpath(XPATH_CREATE_ACCOUNT)).click();
 
-        while (!driver.findElement(By.id("firstname")).isDisplayed());
-        WebElement searchField = driver.findElement(By.id("firstname"));
-        searchField.sendKeys("Firstname");
+        Thread.sleep(5000);
+        WebElement firstnameField = driver.findElement(By.xpath(XPATH_FIRST_NAME));
+        firstnameField.sendKeys("Firstname");
+
+
+        WebElement lastnameField = driver.findElement(By.xpath(XPATH_LAST_NAME));
+        lastnameField.sendKeys("Lastname");
+
+        WebElement emailField = driver.findElement(By.xpath(XPATH_EMAIL));
+        emailField.sendKeys("test@test.ie");
+
+        WebElement mobField = driver.findElement(By.xpath(XPATH_MONTH_OF_BIRTH));
+        Select mobSelect = new Select(mobField);
+        mobSelect.selectByIndex(12);
+
+        WebElement dobField = driver.findElement(By.xpath(XPATH_DAY_OF_BIRTH));
+        Select dobSelect = new Select(dobField);
+        dobSelect.selectByIndex(27);
+
+        WebElement yobField = driver.findElement(By.xpath(XPATH_YEAR_OF_BIRTH));
+        Select yobSelect = new Select(yobField);
+        yobSelect.selectByIndex(29);
+
 
 
 
